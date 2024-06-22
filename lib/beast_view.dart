83846090bc9d186b50c14718cfc3fe9913b474cd
darjaorlova <dariaorlova.bbapps@gmail.com>
2024-06-22 21:05:58 +0300

    Refactor to use a reusable beast widget

---
+++ lib/beast_view.dart
@@ -0,0 +1,123 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
+class BeastView extends StatelessWidget {
+  const BeastView({
+    required this.name,
+    required this.description,
+    required this.beastImage,
+    required this.backgroundImage,
+    required this.backgroundSemanticLabel,
+    super.key,
+  });
+
+  final String name;
+  final String description;
+  final String beastImage;
+  final String backgroundImage;
+  final String backgroundSemanticLabel;
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.sizeOf(context);
+    var maxH = size.height;
+    var maxW = size.width;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              backgroundImage,
+              fit: BoxFit.cover,
+              semanticLabel: backgroundSemanticLabel,
+            ),
+          ),
+          ClipRRect(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 12, sigmaY: 12),
+              child: const SizedBox.expand(),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Transform(
+              transform: Matrix4.translationValues(130, 240, 0),
+              child: OverflowBox(
+                maxHeight: maxH * 2,
+                maxWidth: maxW * 2,
+                child: Image.asset(
+                  beastImage,
+                  height: maxH * 0.85,
+                  fit: BoxFit.contain,
+                  semanticLabel: name,
+                )
+                    .animate()
+                    .fadeIn(
+                      delay: 300.ms,
+                      duration: 1000.ms,
+                      curve: Curves.easeIn,
+                    )
+                    .move(
+                      delay: 300.ms,
+                      duration: 1000.ms,
+                      curve: Curves.easeIn,
+                      begin: const Offset(-100, 20),
+                      end: Offset.zero,
+                    ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 48, right: 32, left: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                      color: Color(0xFFECE8D7),
+                      fontSize: 44,
+                      fontWeight: FontWeight.w700,
+                      fontFamily: 'FantasticBeast',
+                      shadows: [
+                        Shadow(
+                          color: Color(0xFF060B10),
+                          blurRadius: 20,
+                          offset: Offset(5, 5),
+                        ),
+                      ],
+                    ),
+                  ).animate().fadeIn(
+                        delay: 600.ms,
+                        duration: 1000.ms,
+                        curve: Curves.easeIn,
+                      ),
+                  Text(
+                    description,
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                      color: Color(0xFFECE8D7),
+                      fontSize: 18,
+                      height: 1.1,
+                      fontFamily: 'Almendra',
+                    ),
+                  ).animate().fadeIn(
+                        delay: 600.ms,
+                        duration: 1000.ms,
+                        curve: Curves.easeIn,
+                      ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/beasts_page_view.dart
+++ lib/beasts_page_view.dart
@@ -1,34 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:magic_beasts/aqualis_view.dart';
-import 'package:magic_beasts/ignisphinx_view.dart';
-import 'package:magic_beasts/luminara_view.dart';
-import 'package:magic_beasts/shadowfelis_view.dart';
-import 'package:magic_beasts/zephyra_view.dart';
+import 'package:magic_beasts/beast_view.dart';
 
-class BeastsPageView extends StatefulWidget {
+class BeastsPageView extends StatelessWidget {
   const BeastsPageView({super.key});
-
-  @override
-  State<BeastsPageView> createState() => _BeastsPageViewState();
-}
-
-class _BeastsPageViewState extends State<BeastsPageView> {
-  @override
-  void initState() {
-    super.initState();
-    debugPrint('Bad code, called before super.initState() 🐛');
-    // super.initState();
-  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
         body: PageView(
           children: const [
-            ShadowfelisView(),
-            ZephyraView(),
-            AqualisView(),
-            LuminaraView(),
-            IgnisphinxView(),
+            BeastView(
+              name: 'Shadowfelis',
+              description: '''
+The Shadowfelis is a rare and enigmatic magical beast, often found lurking in the depths of ancient, enchanted forests. Resembling a large cat, this creature boasts sleek, mystical fur adorned with dark, shimmering patterns that seem to ripple and change with the light. Its large, hauntingly glowing eyes possess an eerie, otherworldly intelligence, reflecting both wisdom and a hint of mischief.''',
+              beastImage: 'resources/images/shadowfelis.png',
+              backgroundImage: 'resources/images/bg_forest.webp',
+              backgroundSemanticLabel: 'Forest background',
+            ),
+            BeastView(
+              name: 'Zephyra',
+              description: '''
+The Zephyra is a guardian of the enchanted skies it inhabits, bringing peace and tranquility to its surroundings. Legends tell of lost travelers and sailors who were guided to safety by the gentle light of a Zephyra. Its presence is believed to purify and bless the skies, making them a source of healing and magic. Despite its mystical appearance, the Zephyra is a gentle and approachable being, often interacting with those who show respect and reverence for the air and its creatures''',
+              beastImage: 'resources/images/zephyra.png',
+              backgroundImage: 'resources/images/bg_forest_green.webp',
+              backgroundSemanticLabel: 'Green forest background',
+            ),
+            BeastView(
+              name: 'Luminara',
+              description:
+              '''The Luminara is a beacon of light and tranquility in the mystical realms it inhabits. Its presence is said to bring peace and healing to those who encounter it. Legends tell of travelers lost in the dark, enchanted woods who were guided to safety by the gentle glow of a Luminara. Its antlers, inscribed with ancient runes, are believed to possess powerful protective magic, warding off dark spirits and malevolent forces.''',
+              beastImage: 'resources/images/luminara.png',
+              backgroundImage: 'resources/images/bg_forest_pink.webp',
+              backgroundSemanticLabel: 'Pink forest background',
+            ),
+            BeastView(
+              name: 'Aqualis',
+              description:
+                  '''The Aqualis is a guardian of the enchanted waters it inhabits, bringing peace and tranquility to its surroundings. Legends tell of lost sailors and travelers guided to safety by the gentle glow of an Aqualis. Its presence is believed to purify and bless the waters, making them a source of healing and magic. Despite its mystical appearance, the Aqualis is a gentle and approachable being, often interacting with those who show respect and reverence for the water and its creatures. The Aqualis's serene and calming aura makes it a symbol of hope and guidance in the magical world.''',
+              beastImage: 'resources/images/aqualis.png',
+              backgroundImage: 'resources/images/bg_underwater.webp',
+              backgroundSemanticLabel: 'Underwater background',
+            ),
+            BeastView(
+              name: 'Ignisphinx',
+              description:
+                  '''The Ignisphinx is a guardian of the enchanted fire realms it inhabits, bringing both awe and fear to those who encounter it. Legends tell of ancient warriors and travelers who were granted protection and strength by the Ignisphinx. Its presence is believed to purify and bless the surrounding lands, making them a source of powerful magic and vitality. Despite its fiery and intimidating appearance, the Ignisphinx is a noble and wise being, often interacting with those who show courage and respect for the elements. The Ignisphinx's regal and commanding aura makes it a symbol of power and protection in the magical world.''',
+              beastImage: 'resources/images/ignisphinx.png',
+              backgroundImage: 'resources/images/bg_red.webp',
+              backgroundSemanticLabel: 'Red background',
+            ),
           ],
         ),
       );
